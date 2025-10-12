43f4ce8217f5eafc994b5cdfd962168484fbeac6
Kamrul Haider Chowdhury <kamrulhaider.ssb@gmail.com>
2025-10-12 12:51:19 +0600

    feat: update CORS configuration to allow all origins

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import cors from "cors";
 
 export const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors());
 app.use(requestLogger);
 app.use(loadUser);
 
